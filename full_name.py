932a06d52bf340a7e96c7835269aaffa594f7880
Kenneth Simmons <latestnight@protonamil.com>
2022-05-26 15:36:25 -0500

    initial push

---
+++ full_name.py
@@ -0,0 +1,16 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = 'python '
+print(favorite_language)
+print(favorite_language.rstrip())
---
+++ simple_messages.py
@@ -0,0 +1,5 @@
+message = 'i am studying'
+print(message)
+
+message = 'i like to learn'
+print(message)
